c81a74021f19cff9105fe9edce98325d461c0a9e
Frederic Villeneuve <frederic.villeneuve@gmail.com>
2013-05-31 08:19:30 -0400

    Removed unused code

--- src/abstraction/pa/powerpc-604/code/pa_abstract.c
+++ src/abstraction/pa/powerpc-604/code/pa_abstract.c
@@ -13,22 +13,6 @@
  * Implements PA abstraction for x86
  */
 
-/*static inline int   MulleAtomicIncrement( int *p)
-{
-   int    tmp;
-
-   // code lifted from linux
-   asm volatile(
-                "1:     lwarx   %0,0,%1\n"
-                "   addic   %0,%0,1\n"
-                "   stwcx.  %0,0,%1\n"
-                "   bne-    1b"
-                : "=&r" (tmp)
-                : "r" (p)
-                : "cc", "memory");
-
-   return( tmp);
-}*/
 
 os_uint32
 pa_increment(
@@ -37,6 +21,7 @@ pa_increment(
     os_uint32 value;
 
 #ifdef __GNUC__
+    // code lifted from linux
     __asm__ __volatile__ (
     "1: lwarx   %0,0,%1\n"
     "   addic   %0,%0,1\n"
@@ -54,29 +39,13 @@ pa_increment(
 }
 
 
-/*static inline int   MulleAtomicDecrement( int *p)
-{
-   int    tmp;
-
-   // code lifted from linux
-   asm volatile(
-                "1:     lwarx   %0,0,%1\n"
-                "   addic   %0,%0,-1\n"  // addic allows r0, addi doesn't 
-                "   stwcx.  %0,0,%1\n"
-                "   bne-    1b"
-                : "=&r" (tmp)
-                : "r" (p)
-                : "cc", "memory");
-
-   return( tmp);
-}*/
-
 os_uint32
 pa_decrement(
     os_uint32 *count)
 {
     os_uint32 value;
 #ifdef __GNUC__
+    // code lifted from linux
     __asm__ __volatile__(
     "1: lwarx   %0,0,%1\n"
     "   addic   %0,%0,-1\n"
